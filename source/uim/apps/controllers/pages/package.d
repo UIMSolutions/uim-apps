0ec64c14ef5b5293b03b7a43604e530f5d9c8908
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-02-20 17:36:25 +0100

    Bugfix

--- source/uim/apps/controllers/pages/package.d
+++ source/uim/apps/controllers/pages/package.d
@@ -24,22 +24,47 @@ public import uim.apps.controllers.pages.helpers;
 public import uim.apps.controllers.pages.register;
 public import uim.apps.controllers.pages.sites;
 
-template APPPageControllerThis(string name) {
+template APPPageControllerThis(string name, bool withEntity = false, bool withEntities = false) {
   const char[] APPPageControllerThis = `
 this() { super(); this.name("`~name~`"); }
 this(DAPPApplication myApplication) { this().app(myApplication); }
 this(DAPPView myView) { this().view(myView); }
 this(DAPPApplication myApplication, DAPPView myView) { this().view(myView); }
-`;
+`~
+(withEntity ? `
+this(DOOPEntity myEntity) { this().entity(myEntity); }
+this(DAPPApplication myApplication, DOOPEntity myEntity) { this(myApplication).entity(myEntity); }
+this(DAPPView myView, DOOPEntity myEntity) { this(myView).entity(myEntity); }
+this(DAPPApplication myApplication, DAPPView myView, DOOPEntity myEntity) { this(myApplication, myView).entity(myEntity); }
+` : ``)
+~
+(withEntities ? `
+this(DOOPEntity[] myEntities) { this().entities(myEntities); }
+this(DAPPApplication myApplication, DOOPEntity[] myEntities) { this(myApplication).entities(myEntities); }
+this(DAPPView myView, DOOPEntity[] myEntities) { this(myView).entities(myEntities); }
+this(DAPPApplication myApplication, DAPPView myView, DOOPEntity[] myEntities) { this(myApplication, myView).entities(myEntities); }
+` : ``);
 }
-
-template APPPageControllerCalls(string name) {
+template APPPageControllerCalls(string name, bool withEntity = false, bool withEntities = false) {
   const char[] APPPageControllerCalls = `
 auto `~name~`() { return new D`~name~`; }
 auto `~name~`(DAPPApplication myApplication) { return new D`~name~`(myApplication); }
 auto `~name~`(DAPPView myView) { return new D`~name~`(myView); }
 auto `~name~`(DAPPApplication myApplication, DAPPView myView) { return new D`~name~`(myApplication, myView); }
-`;
+`~
+(withEntity ? `
+auto `~name~`(DOOPEntity myEntity) { return new D`~name~`(myEntity); }
+auto `~name~`(DAPPApplication myApplication, DOOPEntity myEntity) { return new D`~name~`(myApplication, myEntity); }
+auto `~name~`(DAPPView myView, DOOPEntity myEntity) { return new D`~name~`(myView, myEntity); }
+auto `~name~`is(DAPPApplication myApplication, DAPPView myView, DOOPEntity myEntity) { return new D`~name~`(myApplication, myView, myEntity); }
+` : ``)
+~
+(withEntities ? `
+auto `~name~`(DOOPEntity[] myEntities) { return new D`~name~`(myEntities); }
+auto `~name~`(DAPPApplication myApplication, DOOPEntity[] myEntities) { return new D`~name~`(myApplication, myEntities); }
+auto `~name~`(DAPPView myView, DOOPEntity[] myEntities) { return new D`~name~`(myView, myEntities); }
+auto `~name~`(DAPPApplication myApplication, DAPPView myView, DOOPEntity[] myEntities) { return new D`~name~`(myApplication, myView, myEntities); }
+` : ``);
 }
 
 // Get page by names
