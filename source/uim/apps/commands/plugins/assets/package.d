33062e32247faf3cb5da80bbe855246611d36b1e
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-31 17:09:03 +0200

    Update

--- source/uim/apps/commands/plugins/assets/package.d
+++ source/uim/apps/commands/plugins/assets/package.d
@@ -1,1 +1,5 @@
 module uim.apps.commands.plugins.assets;
+
+public import uim.apps.commands.plugins.assets.copy;
+public import uim.apps.commands.plugins.assets.remove;
+public import uim.apps.commands.plugins.assets.symlink;
