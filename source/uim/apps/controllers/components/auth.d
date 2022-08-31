fe14100e16dab8560544a9054859d6ebb5c56f38
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-31 17:34:51 +0200

    Add AuthControllerComponent

--- source/uim/apps/controllers/components/auth.d
+++ source/uim/apps/controllers/components/auth.d
@@ -3,7 +3,11 @@ module uim.apps.controllers.auth;
 @safe:
 import uim.apps;
 
-class DAPPControllerRegistry : DControllerComponent
+class DAuthControllerComponent : DControllerComponent
   this() {}  
 }
-auto APPControllerRegistry() { return new DAPPControllerRegistry; }
+auto AuthControllerComponent() { return new DAuthControllerComponent; }
+
+version(test_uim_apps) { unitest {
+  assert(AuthControllerComponent);
+}}
