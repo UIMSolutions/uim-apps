b7d8af390f19f0a9cefb1c24309317227c2ab76b
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-09-22 18:18:19 +0200

    Updates

--- source/uim/apps/object_.d
+++ source/uim/apps/object_.d
@@ -52,8 +52,7 @@ class DAPPObject {
       return cast(O)this;
     }
   // #endregion error
-
-
+  
   O create(this O)() {
     return new O;
   }
@@ -69,7 +68,6 @@ version(test_uim_apps) { unittest {
     writeln("--- Test in ", __MODULE__, "/", __LINE__);
     assert(new DAPPObject);
 
-    writeln("--- Test in ", __MODULE__, "/", __LINE__);
     assert(APPObject);
 
     writeln("--- Test in ", __MODULE__, "/", __LINE__);
