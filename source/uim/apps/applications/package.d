9950a1abc690f159f9a2c20893e100f12875e822
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-11-05 12:57:26 +0100

    Updates

--- source/uim/apps/applications/package.d
+++ source/uim/apps/applications/package.d
@@ -8,9 +8,3 @@ public import uim.apps.applications.application;
 
 // MOdules
 public import uim.apps.applications.registry;
-
-// Global registry for applications
-DAPPApplicationRegistry applicationRegistry;
-static this() {
-  applicationRegistry = APPApplicationRegistry;
-}
