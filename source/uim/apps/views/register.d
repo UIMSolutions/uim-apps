7bb8383adee2c152a3e5de89bc71c4357cc128e7
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-02-17 13:25:18 +0100

    Consolidation uim-app to uim-mvc

---
+++ source/uim/apps/views/register.d
@@ -0,0 +1,15 @@
+/***********************************************************************************************************************
+*	Copyright: © 2017-2022 UI Manufaktur UG / 2022 Ozan Nurettin Süel (sicherheitsschmiede)                              *
+*	License: Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt]                                       *
+*	Authors: UI Manufaktur UG Team, Ozan Nurettin Süel (Sicherheitsschmiede)										                         * 
+***********************************************************************************************************************/
+module uim.apps.views.registry;
+
+@safe:
+import uim.apps;
+
+class DAPPViewRegistry : DRegistry!DAPPView {
+  this() { super(); }
+}
+auto ViewRegistry() { return new DAPPViewRegistry(); }
+
