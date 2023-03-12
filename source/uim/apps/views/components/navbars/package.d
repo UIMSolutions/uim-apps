f4919f975acfe1f73dc937df214328274a959d48
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-12 12:32:29 +0100

    Bugfixing

--- source/uim/apps/views/components/navbars/package.d
+++
@@ -1,5 +0,0 @@
-module uim.apps.views.components.navbars;
-
-public import uim.apps.views.components.navbars.navbar;
-public import uim.apps.views.components.navbars.navigation;
-public import uim.apps.views.components.navbars.slot;
