3bb31341b660b08aed3636bab9425b5d437e3b39
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-12 12:40:46 +0100

    Bugfixing

--- source/uim/apps/views/components/tables/components/bodies/entity.d
+++
@@ -1,9 +0,0 @@
-module uim.apps.views.components.tables.components.bodies.entity;
-
-@safe:
-import uim.apps;
-
-class DAPPEntityTableBody : DAPPTableBody {
-  mixin(ViewComponentThis!("APPEntityTableBody"));
-}
-mixin(ViewComponentCalls!("APPEntityTableBody"));
